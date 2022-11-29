a6117ed568ad89ff3216c331fde4d78cd4c61f96
neanie1 <neavnieminen@gmail.com>
2022-11-29 15:46:04 +0200

    javascript aloitus

---
+++ javascriptit/luontovisa.js
@@ -0,0 +1,18 @@
+//document.getElementById("results-h").onclick = function() {
+   // getResults();
+//}
+
+function getResults(){
+
+let questions = document.getElementsByClassName("questions").innerHTML;
+let correctAnswer = document.getElementsByClassName("correct").innerHTML;
+let wrongAnswer = document.getElementsByClassName("wrong").innerHTML;
+
+
+if (document.getElementById("correct1").checked){
+    document.getElementById("results").innerHTML = "Vastaus on oikein"
+
+}else {
+    document.getElementById("results").innerHTML = "Vastaus on väärin"
+}
+}
