8ee6871d868dda57c2c2a471053218446bb908bb
neanie1 <neavnieminen@gmail.com>
2022-12-01 13:52:40 +0200

    disabled

--- javascriptit/luontovisa.js
+++ javascriptit/luontovisa.js
@@ -7,10 +7,16 @@ function getResults(){
 
 if (document.getElementById("correct1").checked){
     document.getElementById("result1").innerHTML = "Vastaus on oikein"
+        document.getElementById("correct1").disabled = true;
+        document.getElementById("wrong1").disabled = true;
+        document.getElementById("wrong2").disabled = true;
 }
 
 else if( document.getElementById("wrong1").checked || document.getElementById("wrong2").checked) {
     document.getElementById("result1").innerHTML = "Väärin"
+        document.getElementById("correct1").disabled = true;
+        document.getElementById("wrong1").disabled = true;
+        document.getElementById("wrong2").disabled = true;
 }
 
 else {
@@ -19,11 +25,18 @@ else {
 
 
 
+
 if (document.getElementById("correct2").checked){
     document.getElementById("result2").innerHTML = "Vastaus on oikein"
+        document.getElementById("correct2").disabled = true;
+        document.getElementById("wrong3").disabled = true;
+        document.getElementById("wrong4").disabled = true;
 }
     else if( document.getElementById("wrong3").checked || document.getElementById("wrong4").checked) {
         document.getElementById("result2").innerHTML = "Väärin"
+            document.getElementById("correct2").disabled = true;
+            document.getElementById("wrong3").disabled = true;
+            document.getElementById("wrong4").disabled = true;
     }
 
 else {
@@ -34,9 +47,16 @@ else {
 
 if (document.getElementById("correct3").checked){
     document.getElementById("result3").innerHTML = "Vastaus on oikein"
+        document.getElementById("correct3").disabled = true;
+        document.getElementById("wrong5").disabled = true;
+        document.getElementById("wrong6").disabled = true;
+
 
  } else if( document.getElementById("wrong5").checked || document.getElementById("wrong6").checked) {
         document.getElementById("result3").innerHTML = "Väärin"
+            document.getElementById("correct3").disabled = true;
+            document.getElementById("wrong5").disabled = true;
+            document.getElementById("wrong6").disabled = true;
     }
 
 else {
@@ -47,9 +67,15 @@ else {
 
 if (document.getElementById("correct4").checked){
     document.getElementById("result4").innerHTML = "Vastaus on oikein"
+        document.getElementById("correct4").disabled = true;
+        document.getElementById("wrong7").disabled = true;
+        document.getElementById("wrong8").disabled = true;
 
  } else if( document.getElementById("wrong7").checked || document.getElementById("wrong8").checked) {
         document.getElementById("result4").innerHTML = "Väärin"
+            document.getElementById("correct4").disabled = true;
+            document.getElementById("wrong7").disabled = true;
+            document.getElementById("wrong8").disabled = true;
     }
 
 else {
@@ -58,9 +84,15 @@ else {
 
 if (document.getElementById("correct5").checked){
     document.getElementById("result5").innerHTML = "Vastaus on oikein"
+        document.getElementById("correct5").disabled = true;
+        document.getElementById("wrong9").disabled = true;
+        document.getElementById("wrong10").disabled = true;
 
  } else if( document.getElementById("wrong9").checked || document.getElementById("wrong10").checked) {
         document.getElementById("result5").innerHTML = "Väärin"
+            document.getElementById("correct5").disabled = true;
+            document.getElementById("wrong9").disabled = true;
+            document.getElementById("wrong10").disabled = true;
     }
 
 else {
