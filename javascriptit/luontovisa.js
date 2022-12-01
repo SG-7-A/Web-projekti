2f1589bc238561743dab22580021232d7e1f9393
neanie1 <neavnieminen@gmail.com>
2022-12-01 13:16:49 +0200

    else if else if

--- javascriptit/luontovisa.js
+++ javascriptit/luontovisa.js
@@ -5,20 +5,66 @@ const NO_ANSWER = ""
 
 function getResults(){
 
-let questions = document.getElementsByClassName("questions").innerHTML;
-let correctAnswer = document.getElementsByClassName("correct").innerHTML;
-let wrongAnswer = document.getElementsByClassName("wrong").innerHTML;
-
-
 if (document.getElementById("correct1").checked){
-    document.getElementById("results").innerHTML = "Vastaus on oikein"
+    document.getElementById("result1").innerHTML = "Vastaus on oikein"
+}
 
-}else if (document.getElementsByClassName("wrong").checked){
-    document.getElementById("results").innerHTML = "Vastaus on väärin"
+else if( document.getElementById("wrong1").checked || document.getElementById("wrong2").checked) {
+    document.getElementById("result1").innerHTML = "Väärin"
+}
 
-} else {
-    document.getElementById("results").innerHTML = NO_ANSWER;
+else {
+    document.getElementById("result1").innerHTML = NO_ANSWER;
 }
 
 
+
+if (document.getElementById("correct2").checked){
+    document.getElementById("result2").innerHTML = "Vastaus on oikein"
+}
+    else if( document.getElementById("wrong3").checked || document.getElementById("wrong4").checked) {
+        document.getElementById("result2").innerHTML = "Väärin"
+    }
+
+else {
+    document.getElementById("result2").innerHTML = NO_ANSWER
+
+}
+
+
+if (document.getElementById("correct3").checked){
+    document.getElementById("result3").innerHTML = "Vastaus on oikein"
+
+ } else if( document.getElementById("wrong5").checked || document.getElementById("wrong6").checked) {
+        document.getElementById("result3").innerHTML = "Väärin"
+    }
+
+else {
+    document.getElementById("result3").innerHTML = NO_ANSWER
+
+}
+
+
+if (document.getElementById("correct4").checked){
+    document.getElementById("result4").innerHTML = "Vastaus on oikein"
+
+ } else if( document.getElementById("wrong7").checked || document.getElementById("wrong8").checked) {
+        document.getElementById("result4").innerHTML = "Väärin"
+    }
+
+else {
+    document.getElementById("result4").innerHTML = NO_ANSWER
+}
+
+if (document.getElementById("correct5").checked){
+    document.getElementById("result5").innerHTML = "Vastaus on oikein"
+
+ } else if( document.getElementById("wrong9").checked || document.getElementById("wrong10").checked) {
+        document.getElementById("result5").innerHTML = "Väärin"
+    }
+
+else {
+    document.getElementById("result5").innerHTML = NO_ANSWER
+
+}
 }
