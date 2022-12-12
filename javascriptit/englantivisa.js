bda68c97b93546f6d661ca88c36d9499a9416b21
ememilia <n2vaem00sstudents.oamk.fi>
2022-12-12 11:37:06 +0200

    javascript anglantivisaan, oikeat vastaukset taulukko html:lään

--- javascriptit/englantivisa.js
+++ javascriptit/englantivisa.js
@@ -1,54 +1,32 @@
+
+// Vakio muuttuja taulukko mihin talletetaan käyttäjän valinnat
+const USER_ANSWERS = [];
 // Vakio muuttuja kysymyksille
 const QUESTIONS = [
   {
     question: "1. Mitä tarkoittaa clock tai watch?",
-    answers: {
-      a: "Katso",
-      b: "Me",
-      c: "Kello",
-      d: "Avaruus",
-    },
-    correctAnswer: "c",
+    answers: ["Katso", "Me", "Kello", "Avaruus"],
+    correctAnswer: 2,
   },
   {
     question: "2. Miten kirjoitetaan Orava englanniksi?",
-    answers: {
-      a: "Octopuss",
-      b: "Squirrel",
-      c: "Bird",
-      d: "beaver",
-    },
-    correctAnswer: "b",
+    answers: ["Octopuss", "Squirrel", "Bird", "Beaver"],
+    correctAnswer: 1,
   },
   {
-    question: "3. Mikä on oikea lyhenne i am:lle",
-    answers: {
-      a: "Am",
-      b: "I'a",
-      c: "I",
-      d: "I'm",
-    },
-    correctAnswer: "d",
+    question: "3. Mikä on oikea lyhenne i am:lle eli minä olen?",
+    answers: ["Am", "Is", "I", "I'm"],
+    correctAnswer: 3,
   },
   {
-    question: "4. Mikä hedelmä kyseessä?",
-    answers: {
-      a: "Apple",
-      b: "Kiwi",
-      c: "Watermelon",
-      d: "Orange",
-    },
-    correctAnswer: "a",
+    question: "4. Mikä on omena englanniksi?", 
+    answers: ["Apple", "Kiwi", "Watermelon", "Orange"],
+    correctAnswer: 0,
   },
   {
     question: "5. Miten kirjoitetaan minun nimeni on?",
-    answers: {
-      a: "My age is",
-      b: "My name is",
-      c: "My dogs name is",
-      d: "My address is",
-    },
-    correctAnswer: "b",
+    answers: ["My age is", "My name is", "My dogs name is", "My address is"],
+    correctAnswer: 1,
   },
 ];
 
@@ -60,62 +38,98 @@ const ANSWER_B = document.getElementById("b");
 const ANSWER_C = document.getElementById("c");
 const ANSWER_D = document.getElementById("d");
 // Vakio diville missä valintanapit
-const ANSWER_BUTTONS = [
-  ANSWER_A,
-  ANSWER_B,
-  ANSWER_C,
-  ANSWER_D
-]
+const ANSWER_BUTTONS = [ANSWER_A, ANSWER_B, ANSWER_C, ANSWER_D];
 // Vakio muuttuja submit ja seuraava kysymys
 const NEXT_BUTTON = document.getElementById("next-button");
 // index muuttuja kysymystaulukkoa varten
-let index = 0;
+
 let quizScore = 0;
-//
 
-function selectedAnswer(){
-  let clickedAnswer = undefined;
-  ANSWER_BUTTONS.forEach((element) =>{
-    if(element.clicked){
-      clickedAnswer = element.id;
-    }
+const QUESTION_EXPLAIN = document.getElementById("question-explain");
+
+const saveUserAnswer = (userSelection) => {
+  let answerIndex = userSelection;
+  let correctIndex = QUESTIONS[questionIndex].correctAnswer;
+
+  // toisen tehtävän lisää elementti tässä
+  let newChild = document.createElement("div");
+  newChild.innerText = "fOOBARR";
+  QUESTION_EXPLAIN.appendChild(newChild);
+
+  ANSWER_BUTTONS.forEach((button) => {
+    // disabloi kaikki napit
+    // tarkoittaa hmtl:ssä <button disabled="true"/>
+    // siksi koska vastaukseen ei pitäisi pystyä vastaamaan
+    // useamman kerran
+    button.setAttribute("disabled", true);
   });
-  return clickedAnswer;
-}
-// Poistaa valinnan
-function deleteSelected(){
-  ANSWER_BUTTONS.forEach((element) => {
-    element.checked = false;
+  // poistetaan disaple next-buttonista
+  NEXT_BUTTON.removeAttribute("disabled");
+
+  if (correctIndex === answerIndex) {
+    quizScore = quizScore + 1;
+    
+  }
+
+  [correctIndex, answerIndex].forEach((idx) => {
+    // toisen tehtävän luokkalistan päivitys
+    const classToAdd = correctIndex === idx ? "correct" : "wrong";
+    ANSWER_BUTTONS[idx].classList.add(classToAdd);
   });
-}
+};
+
 // Näyttää kysymykset ja vastaukset
-function showQuiz(){
-  deleteSelected();
-  QUESTION_PLACE.innerText = QUESTIONS[index].question;
-  ANSWER_A.innerText = QUESTIONS[index].answers.a;
-  ANSWER_B.innerText = QUESTIONS[index].answers.b;
-  ANSWER_C.innerText = QUESTIONS[index].answers.c;
-  ANSWER_D.innerText = QUESTIONS[index].answers.d;
-}
-// Näyttää seuraavan kysymyksen sekä vastaukset
-function nextGuestion(){
-  NEXT_BUTTON.addEventListener("click", () => {
-    let pressed = selectedAnswer();
-    if  (pressed ){
-      if( pressed == QUESTIONS[index].correctAnswer){
-        quizScore++;
-      }
-    }
-    index++;
-    if ( index < QUESTIONS.lenght ){
-      nextGuestion();
-    } else{
-      document.getElementById("results").innerHTML = "Tässä visailun tulos" + quizScore + "/5";
-    }
-  })
-}
-function jotain(){
+function showQuestion(questionNumber) {
+  const q = QUESTIONS[questionNumber];
 
+  const explainChild = QUESTION_EXPLAIN.children[0];
+
+  if (explainChild) QUESTION_EXPLAIN.removeChild(explainChild);
+
+  QUESTION_PLACE.innerText = q.question;
+  // disabloi next buttonin
+  NEXT_BUTTON.setAttribute("disabled", true);
+  ANSWER_BUTTONS.forEach((button, buttonIndex) => {
+    // jokainen nappi pitää alustaa
+    // eli siis poistaa luokat wrong/correct
+    button.innerText = q.answers[buttonIndex];
+    button.classList.remove("wrong");
+    button.classList.remove("correct");
+    button.removeAttribute("disabled");
+  });
 }
-showQuiz();
-nextGuestion();
+
+// visan aloitustila
+let questionIndex = 0;
+
+const startQuiz = () => {
+  ANSWER_BUTTONS.forEach((button, buttonIndex) => {
+    button.addEventListener("click", () => {
+      // enabloi kaikki napit
+      // tarkoittaa hmtl:ssä <button disabled="false"/>
+      saveUserAnswer(buttonIndex);
+    });
+    // lataa ensimmäinen kysymys
+    showQuestion(questionIndex);
+  });
+};
+
+const nextQuestion = () => {
+  // lataa vain jos lisää kysymyksiä
+  const moreQuestions = QUESTIONS.length - 1 > questionIndex;
+  if (moreQuestions) {
+    const nextQuestion = questionIndex + 1;
+    showQuestion(nextQuestion);
+    questionIndex = nextQuestion;
+  } else {
+    NEXT_BUTTON.innerText = "Tulos";
+    // Tulostaa visailun pistemäärän
+    document.getElementById("results").innerHTML = "Onneksi olkoot sait " + quizScore + "/5 pistettä"
+    // jos ei enempää kysymyksiä näyttää vastauksille oikeat vastaukset
+    let quizEnding = document.getElementById("allAnswers");
+    quizEnding.style.display = "block";
+  }
+};
+
+document.addEventListener("DOMContentLoaded", startQuiz);
+NEXT_BUTTON.addEventListener("click", nextQuestion);
